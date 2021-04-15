dd448677a1281d8b3557a710f227537e4a2ae099
Jack Ord <36296721+j-or@users.noreply.github.com>
2021-04-15 16:32:55 +0200

    Fix spacing issue with options in optimizer model

--- components/optimizer-model.tsx
+++ components/optimizer-model.tsx
@@ -1,5 +1,7 @@
 import { Button, Table, TableBody, TableCell, TableHead, TableRow, Typography } from '@material-ui/core'
 import { CategoricalVariableType, ExperimentType, ValueVariableType } from '../types/common'
+import DeleteIcon from '@material-ui/icons/Delete'
+import { ReactNode } from 'react'
 
 type OptimizerModelProps = {
   experiment: ExperimentType
@@ -10,6 +12,18 @@ type OptimizerModelProps = {
 export default function OptimizerModel(props: OptimizerModelProps) {
 
   const { experiment: { valueVariables, categoricalVariables} } = props
+
+  function renderCategoricalVariableOptions(options: string[]): ReactNode[] {
+    return options.map((option, optionIndex) => {
+      const separator: ReactNode = optionIndex < options.length - 1 ? <br /> : null
+      return (
+        <div key={optionIndex}>
+          <Typography variant="body2">{option}</Typography>
+          {separator}
+        </div>
+      )
+    })
+  }
 
   return (
       <>
@@ -36,7 +50,9 @@ export default function OptimizerModel(props: OptimizerModelProps) {
                   <TableCell align="right">{valueVar.minVal}</TableCell>
                   <TableCell align="right">{valueVar.maxVal}</TableCell>
                   <TableCell align="right">
-                    <Button size="small" onClick={() => {props.onDeleteValueVariable(valueVar)}}>Delete</Button>
+                    <Button size="small" onClick={() => {props.onDeleteValueVariable(valueVar)}}>
+                      <DeleteIcon color="primary" fontSize="small"/>Delete
+                    </Button>
                   </TableCell>
                 </TableRow>
               ))}
@@ -63,12 +79,12 @@ export default function OptimizerModel(props: OptimizerModelProps) {
                     <TableCell component="th" scope="row">{catVar.name}</TableCell>
                     <TableCell align="left">{catVar.description}</TableCell>
                     <TableCell align="left">
-                      {catVar.options.map((option, optionIndex) => (
-                        <div key={optionIndex}><Typography variant="body2">{option}</Typography><br/></div>
-                      ))}
+                      {renderCategoricalVariableOptions(catVar.options)}
                     </TableCell>
                     <TableCell align="right">
-                      <Button size="small" onClick={() => {props.onDeleteCategoricalVariable(catVar)}}>Delete</Button>
+                      <Button size="small" onClick={() => {props.onDeleteCategoricalVariable(catVar)}}>
+                        <DeleteIcon color="primary" fontSize="small"/>Delete
+                      </Button>
                     </TableCell>
                   </TableRow>
                 ))}
