4927b128849a5c8c76fed822e83d329cfdb7d7bf
Jakob Langdal <jakob.langdal@alexandra.dk>
2021-06-29 12:08:52 +0200

    Properly display suggestion count

--- components/experiment.tsx
+++ components/experiment.tsx
@@ -162,7 +162,7 @@ export default function Experiment(props: ExperimentProps) {
                                                 <TextField
                                                     type="number"
                                                     margin="dense"
-                                                    defaultValue={experiment?.extras?['experimentSuggestionCount'] : 1 }
+                                                    defaultValue={experiment?.extras['experimentSuggestionCount'] || 1 }
                                                     name="numberOfSuggestions"
                                                     label="Number of suggestions"
                                                     inputRef={() => {}}
