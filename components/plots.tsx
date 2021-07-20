38eded2972ce991e69992f04f52c4fa488de4e63
AkselObdrup <obdrupaksel@gmail.com>
2021-07-20 08:57:12 +0200

    Help text for plots and width of plots

--- components/plots.tsx
+++ components/plots.tsx
@@ -9,7 +9,18 @@ export const Plots = () => {
       <TitleCard title="Plots">
         {experiment.results.plots.length > 0 ?
           <ul>
-              {experiment.results.plots && experiment.results.plots.map(plot => <li key={plot.id}><img src={`data:image/png;base64, ${plot.plot}`} alt={plot.id}></img></li>)}
+              <h3>Convergence plot</h3>
+              <p>The convegence plot displays the score of the best observation as a function of the number of calls.</p>
+              {experiment.results.plots.filter(plot => plot.id == "convergence").map(plot => <li key={plot.id}><img src={`data:image/png;base64, ${plot.plot}`} width="800" alt={plot.id}></img></li>)}
+              
+              <h3>Objective plot</h3>
+              <p>The objective plot displays the model approximating the objective function.<br/>
+              In the diagonal the dependence of the model on each input variable is shown.
+              For each input variable the other input variables are set by the best observation.<br/>
+              In the lower triangle the pairwise dependencies of the model on each pair of input variables.
+              For each pair the other input variables are set by the best observation.<br/>
+              The best observation is marked with a red dot while the remaining observations are marked with orange dots.</p>
+               {experiment.results.plots.filter(plot => plot.id == "objective").map(plot => <li key={plot.id}><img src={`data:image/png;base64, ${plot.plot}`} width="800" alt={plot.id}></img></li>)}
           </ul>
           :
           "Plots will appear here"
