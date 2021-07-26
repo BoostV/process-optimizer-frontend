7c9b870ad26b6d677f733499c9d2b24e4def8471
Jack Ord <36296721+j-or@users.noreply.github.com>
2021-07-26 10:51:51 +0200

    Test variable order of space converter

--- utility/converters.ts
+++ utility/converters.ts
@@ -2,7 +2,6 @@ import { ExperimentData } from "../openapi"
 import { CategoricalVariableType, DataPointType, ExperimentType, ScoreDataPointType, SpaceType, ValueVariableType } from "../types/common"
 
 export const calculateSpace = (experiment: ExperimentType): SpaceType => {
-
     const numerical: SpaceType = experiment.valueVariables.map(v => { const type = v.discrete ? "discrete" : "continuous"
     return {type, name: v.name, from: Number(v.minVal), to: Number(v.maxVal)}})
     const categorical: SpaceType = experiment.categoricalVariables.map((v) => { return {type: "category", name: v.name, categories: v.options} })
@@ -10,6 +9,6 @@ export const calculateSpace = (experiment: ExperimentType): SpaceType => {
 }
 
 const numPat = / [0-9] + /
-export const calculateData = (categorialValues: CategoricalVariableType[], numericValues: ValueVariableType[], dataPoints: DataPointType[][]): ExperimentData[] => {
+export const calculateData = (categoricalValues: CategoricalVariableType[], numericValues: ValueVariableType[], dataPoints: DataPointType[][]): ExperimentData[] => {
     return dataPoints.map((run):ExperimentData => ({xi: run.filter(it => it.name !== "score").map(it => numericValues.find(p => p.name === it.name) ? Number(it.value) : it.value), yi: Number((run.filter(it => it.name === "score")[0] as ScoreDataPointType).value[0])}))
 }
