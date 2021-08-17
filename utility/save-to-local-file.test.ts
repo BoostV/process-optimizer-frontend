8fa621c1e69468917747bf2934b7c5cb812b9b1e
Jakob Langdal <jakob.langdal@alexandra.dk>
2021-08-17 08:39:45 +0200

    Formatting

--- utility/save-to-local-file.test.ts
+++ utility/save-to-local-file.test.ts
@@ -20,30 +20,31 @@ describe('save-to-local-file', () => {
 
   describe('saveCSVToLocalFile', () => {
     it('Saves CSV', () => {
-        const payload = 'hello,world'
-        const link: any = {
+      const payload = 'hello,world'
+      const link: any = {
         click: jest.fn(),
-        }
-        jest.spyOn(document, 'createElement').mockImplementation(() => link)
-        saveCSVToLocalFile(payload, 'file.csv')
-        expect(link.download).toEqual('file.csv')
-        expect(link.href).toEqual('data:text/csv;charset=utf-8;,hello%2Cworld')
-        expect(link.click).toHaveBeenCalledTimes(1)
-        })
+      }
+      jest.spyOn(document, 'createElement').mockImplementation(() => link)
+      saveCSVToLocalFile(payload, 'file.csv')
+      expect(link.download).toEqual('file.csv')
+      expect(link.href).toEqual('data:text/csv;charset=utf-8;,hello%2Cworld')
+      expect(link.click).toHaveBeenCalledTimes(1)
+    })
   })
+  
   describe('saveObjectToLocalFile', () => {
     it('Saves JSON', () => {
-        const payload = {
-            name: 'test'
-        }
-        const link: any = {
+      const payload = {
+        name: 'test'
+      }
+      const link: any = {
         click: jest.fn(),
-        }
-        jest.spyOn(document, 'createElement').mockImplementation(() => link)
-        saveObjectToLocalFile(payload, 'file.json')
-        expect(link.download).toEqual('file.json')
-        expect(link.href).toEqual('data:application/json;charset=utf-8;,%7B%0A%20%20%22name%22%3A%20%22test%22%0A%7D')
-        expect(link.click).toHaveBeenCalledTimes(1)
-        })
+      }
+      jest.spyOn(document, 'createElement').mockImplementation(() => link)
+      saveObjectToLocalFile(payload, 'file.json')
+      expect(link.download).toEqual('file.json')
+      expect(link.href).toEqual('data:application/json;charset=utf-8;,%7B%0A%20%20%22name%22%3A%20%22test%22%0A%7D')
+      expect(link.click).toHaveBeenCalledTimes(1)
+    })
   })
 })
