3901ca336e840bf3d3a917db76b1c0414c89a47b
Jack Ord <36296721+j-or@users.noreply.github.com>
2021-09-01 10:36:45 +0200

    Change overflow to auto

--- styles/suggestions.style.ts
+++ styles/suggestions.style.ts
@@ -2,7 +2,7 @@ import { makeStyles } from "@material-ui/core";
 
 export const useStyles = makeStyles(theme => ({
   tableContainer: {
-    overflowX: 'scroll',
+    overflowX: 'auto',
   },
   cell: {
     minWidth: 48,
