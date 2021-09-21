44ddbdf62401fdc8c227e993e66969316611058c
Jakob Langdal <jakob.langdal@alexandra.dk>
2021-09-21 14:56:28 +0200

    Update categorical form

--- components/input-model/categorical-variable.tsx
+++ components/input-model/categorical-variable.tsx
@@ -5,6 +5,7 @@ import { useForm } from 'react-hook-form';
 import CategoricalVariableOptions from './categorical-variable-options';
 import { useStyles } from './categorical-variable.style';
 import { CategoricalVariableType } from '../../types/common';
+import { FormInputText } from '../../utility/forms';
 
 type CategoricalVariableProps = {
   isDisabled: boolean
@@ -16,9 +17,9 @@ export default function CategoricalVariable(props: CategoricalVariableProps) {
   const [options, setOptions] = useState([])
   const { isDisabled, onAdded } = props
 
-  const { register, handleSubmit, reset } = useForm<CategoricalVariableType>();
+  const { handleSubmit, reset, control } = useForm<CategoricalVariableType>();
   const onSubmit = async (data: CategoricalVariableType) => {
-    onAdded({...data, options})
+    onAdded({ ...data, options })
     setOptions([])
     reset()
   }
@@ -30,44 +31,46 @@ export default function CategoricalVariable(props: CategoricalVariableProps) {
   }
 
   return (
-      <>
-        <form onSubmit={handleSubmit(onSubmit)}>
-          <TextField 
-            fullWidth
-            margin="dense"
-            label="Name"
-            {...register("name")}
-            />
-          <TextField
-            fullWidth
-            margin="dense"
-            label="Description"
-            {...register("description")}
-          />
-          
-          <Box mt={2}>
-            <Typography>Options</Typography>
-            {options.map((option, index) => (
-              <div key={index}>
-                <div className={classes.option}>
-                  <Typography variant="body1">{option}</Typography>
-                  <IconButton onClick={() => deleteOption(index)} size="small" aria-label="delete" color="primary">
-                    <DeleteIcon fontSize="small" /> 
-                  </IconButton>
-                </div>
+    <>
+      <form onSubmit={handleSubmit(onSubmit)}>
+        <FormInputText
+          name="name"
+          control={control}
+          fullWidth
+          margin="dense"
+          label="Name"
+        />
+        <FormInputText
+          name="description"
+          control={control}
+          fullWidth
+          margin="dense"
+          label="Description"
+        />
+
+        <Box mt={2}>
+          <Typography>Options</Typography>
+          {options.map((option, index) => (
+            <div key={index}>
+              <div className={classes.option}>
+                <Typography variant="body1">{option}</Typography>
+                <IconButton onClick={() => deleteOption(index)} size="small" aria-label="delete" color="primary">
+                  <DeleteIcon fontSize="small" />
+                </IconButton>
               </div>
-            ))}
-          </Box>
+            </div>
+          ))}
+        </Box>
 
-          <CategoricalVariableOptions onOptionAdded={(option: String) => {
-            setOptions([...options, option])
-          }}/>
-          
-          <Box mt={2}>
-            <Button disabled={isDisabled} size="small" variant="outlined" type="submit">Add variable</Button>
-          </Box>
+        <CategoricalVariableOptions onOptionAdded={(option: String) => {
+          setOptions([...options, option])
+        }} />
 
-        </form>
+        <Box mt={2}>
+          <Button disabled={isDisabled} size="small" variant="outlined" type="submit">Add variable</Button>
+        </Box>
+
+      </form>
     </>
   )
 }
