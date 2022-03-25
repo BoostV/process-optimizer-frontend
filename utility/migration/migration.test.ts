f8d758894a359a7af2c53bdbc42554fd8afd7ece
Jakob Langdal <jakob.langdal@alexandra.dk>
2022-03-25 20:10:55 +0000

    Fix file loading in test

--- utility/migration/migration.test.ts
+++ utility/migration/migration.test.ts
@@ -1,64 +1,78 @@
-import { migrate, MIGRATIONS } from "./migration"
+import { migrate, MIGRATIONS } from './migration'
 import version4 from './data-formats/4.json'
 import version3 from './data-formats/3.json'
 import version2 from './data-formats/2.json'
 import version1 from './data-formats/1.json'
-import { emptyExperiment } from "../../store"
-import { ExperimentType } from "../../types/common"
+import { emptyExperiment } from '../../store'
+import { ExperimentType } from '../../types/common'
 import fs from 'fs'
 
 const loadLatestJson = () => {
-  const fileVersions: number[] = fs.readdirSync('utility/migration/data-formats').map(f => parseInt(f.slice('.')[0]))
+  const fileVersions: number[] = fs
+    .readdirSync('utility/migration/data-formats')
+    .map(f => parseInt(f.replace(/[^0-9]/, '')))
   const latestVersion = Math.max(...fileVersions)
-  return JSON.parse(fs.readFileSync(`utility/migration/data-formats/${latestVersion}.json`, { encoding: "utf8", flag: "r" }))
+  return JSON.parse(
+    fs.readFileSync(`utility/migration/data-formats/${latestVersion}.json`, {
+      encoding: 'utf8',
+      flag: 'r',
+    })
+  )
 }
 
-describe("migration", () => {
-  describe("migrate", () => {
-    it("should not migrate if version is newer or equal to latest data format json", async () => {
+describe('migration', () => {
+  describe('migrate', () => {
+    it('should not migrate if version is newer or equal to latest data format json', async () => {
       const latestJson = await loadLatestJson()
-      const jsonNoMigration = { ...latestJson, info: { ...latestJson.info, dataFormatVersion: "10000" } }
+      const jsonNoMigration = {
+        ...latestJson,
+        info: { ...latestJson.info, dataFormatVersion: '10000' },
+      }
       expect(migrate(jsonNoMigration)).toEqual(jsonNoMigration)
       expect(migrate({ ...latestJson })).toEqual({ ...latestJson })
     })
 
-    it("should migrate to 3 from 1 (before versioning and no discrete/continuous)", () => {
-      expect(migrate(version1, "3")).toEqual({
+    it('should migrate to 3 from 1 (before versioning and no discrete/continuous)', () => {
+      expect(migrate(version1, '3')).toEqual({
         ...version3,
         info: {
           ...version3.info,
-          dataFormatVersion: version3.info.dataFormatVersion
+          dataFormatVersion: version3.info.dataFormatVersion,
         },
         valueVariables: version3.valueVariables.map(v => {
           return {
             ...v,
-            type: 'continuous'
+            type: 'continuous',
           }
-        })
+        }),
       })
     })
 
-    it("should migrate to 3 from 2 (before versioning and with discrete as boolean instead of string)", () => {
-      expect(migrate(version2, "3")).toEqual(version3)
+    it('should migrate to 3 from 2 (before versioning and with discrete as boolean instead of string)', () => {
+      expect(migrate(version2, '3')).toEqual(version3)
     })
 
-    it("should migrate to 4 from 3 (expectedMinimum added to result)", () => {
-      expect(migrate(version3, "4")).toEqual(version4)
+    it('should migrate to 4 from 3 (expectedMinimum added to result)', () => {
+      expect(migrate(version3, '4')).toEqual(version4)
     })
 
-    it(`should migrate to newest version (${MIGRATIONS.slice(-1)[0].version})`, async () => {
+    it(`should migrate to newest version (${
+      MIGRATIONS.slice(-1)[0].version
+    })`, async () => {
       const expected = loadLatestJson() as ExperimentType
-      const actual = migrate({...version2})
+      const actual = migrate({ ...version2 })
       expect(actual).toEqual(expected)
     })
   })
 
-  describe("experiment properties", () => {
+  describe('experiment properties', () => {
     //TODO: More/better tests
-    it("newest data format json should match default empty experiment", () => {
-      expect(Object.keys(emptyExperiment).length).toBe(Object.keys(version3).length)
+    it('newest data format json should match default empty experiment', () => {
+      expect(Object.keys(emptyExperiment).length).toBe(
+        Object.keys(version4).length
+      )
       Object.keys(emptyExperiment).forEach(p =>
-        expect(version3).toHaveProperty(p)
+        expect(version4).toHaveProperty(p)
       )
     })
   })
