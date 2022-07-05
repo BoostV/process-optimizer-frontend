aa5ce358719daddbcc5702e67d1a55878af4ee75
Jakob Langdal <jakob.langdal@alexandra.dk>
2022-07-05 12:13:10 +0000

    Refactor reducer test

--- src/reducers/reducers.test.ts
+++ src/reducers/reducers.test.ts
@@ -60,200 +60,216 @@ describe('experiment reducer', () => {
     },
   }
 
-  it('should update whole experiment', async () => {
-    const payload: ExperimentType = {
-      id: '5678',
-      changedSinceLastEvaluation: false,
-      info: {
-        swVersion: '',
-        name: 'Not cake',
-        description: 'Not yummy',
-        dataFormatVersion: '1.0.0',
-      },
-      categoricalVariables: [
-        {
-          name: 'Not icing',
-          description: 'Not sugary',
-          options: [],
-        },
-      ],
-      valueVariables: [
-        {
-          type: 'continuous',
-          name: 'Not water',
-          description: 'Not wet',
-          min: 101,
-          max: 201,
-        },
-      ],
-      scoreVariables: [],
-      optimizerConfig: {
-        baseEstimator: 'GP',
-        acqFunc: 'gp_hedge',
-        initialPoints: 4,
-        kappa: 1.96,
-        xi: 0.01,
-      },
-      results: {
-        id: '123',
-        next: [],
-        plots: [],
-        pickled: '123',
-        expectedMinimum: [],
-        extras: {},
-      },
-      dataPoints: [],
-      extras: {
-        experimentSuggestionCount: 1,
-      },
-    }
-
-    const action: ExperimentAction = {
-      type: 'updateExperiment',
-      payload,
-    }
-
-    expect(rootReducer(initState, action)).toEqual({
-      experiment: payload,
-    })
-  })
-
-  it('should update name', async () => {
-    const action: ExperimentAction = {
-      type: 'updateExperimentName',
-      payload: 'Muffins',
-    }
-
-    expect(rootReducer(initState, action)).toEqual({
-      experiment: {
-        ...initState.experiment,
-        id: '1234',
+  describe('updateExperiment', () => {
+    it('should update whole experiment', async () => {
+      const payload: ExperimentType = {
+        id: '5678',
+        changedSinceLastEvaluation: false,
         info: {
-          ...initState.experiment.info,
-          name: 'Muffins',
+          swVersion: '',
+          name: 'Not cake',
+          description: 'Not yummy',
+          dataFormatVersion: '1.0.0',
         },
-      },
-    })
-  })
-
-  it('should update description', async () => {
-    const action: ExperimentAction = {
-      type: 'updateExperimentDescription',
-      payload: 'Tasty',
-    }
-
-    expect(rootReducer(initState, action)).toEqual({
-      experiment: {
-        ...initState.experiment,
-        info: {
-          ...initState.experiment.info,
-          description: 'Tasty',
-        },
-      },
-    })
-  })
-
-  it('should add value variable', async () => {
-    const payload: ValueVariableType = {
-      type: 'continuous',
-      name: 'Flour',
-      description: 'Wet',
-      min: 300,
-      max: 400,
-    }
-
-    const action: ExperimentAction = {
-      type: 'addValueVariable',
-      payload,
-    }
-
-    expect(rootReducer(initState, action)).toEqual({
-      experiment: {
-        ...initState.experiment,
-        changedSinceLastEvaluation: true,
-        valueVariables: [
-          {
-            name: 'Water',
-            description: 'Wet',
-            type: 'continuous',
-            min: 100,
-            max: 200,
-          },
-          payload,
-        ],
-      },
-    })
-  })
-
-  it('should delete value variable', async () => {
-    const payload: ValueVariableType = {
-      type: 'continuous',
-      name: 'Water',
-      description: 'Wet',
-      min: 100,
-      max: 200,
-    }
-
-    const action: ExperimentAction = {
-      type: 'deleteValueVariable',
-      payload,
-    }
-
-    expect(rootReducer(initState, action)).toEqual({
-      experiment: {
-        ...initState.experiment,
-        changedSinceLastEvaluation: true,
-        valueVariables: [],
-      },
-    })
-  })
-
-  it('should add categorial variable', async () => {
-    const payload: CategoricalVariableType = {
-      name: 'Fat',
-      description: 'Fatty',
-      options: [],
-    }
-
-    const action: ExperimentAction = {
-      type: 'addCategorialVariable',
-      payload,
-    }
-
-    expect(rootReducer(initState, action)).toEqual({
-      experiment: {
-        ...initState.experiment,
-        changedSinceLastEvaluation: true,
         categoricalVariables: [
           {
-            name: 'Icing',
-            description: 'Sugary',
+            name: 'Not icing',
+            description: 'Not sugary',
             options: [],
           },
-          payload,
         ],
-      },
+        valueVariables: [
+          {
+            type: 'continuous',
+            name: 'Not water',
+            description: 'Not wet',
+            min: 101,
+            max: 201,
+          },
+        ],
+        scoreVariables: [],
+        optimizerConfig: {
+          baseEstimator: 'GP',
+          acqFunc: 'gp_hedge',
+          initialPoints: 4,
+          kappa: 1.96,
+          xi: 0.01,
+        },
+        results: {
+          id: '123',
+          next: [],
+          plots: [],
+          pickled: '123',
+          expectedMinimum: [],
+          extras: {},
+        },
+        dataPoints: [],
+        extras: {
+          experimentSuggestionCount: 1,
+        },
+      }
+
+      const action: ExperimentAction = {
+        type: 'updateExperiment',
+        payload,
+      }
+
+      expect(rootReducer(initState, action)).toEqual({
+        experiment: payload,
+      })
     })
   })
 
-  it('should delete categorical variable', async () => {
-    const payload: CategoricalVariableType = {
-      name: 'Icing',
-      description: 'Sugary',
-      options: [],
-    }
+  describe('updateExperimentName', () => {
+    it('should update name', async () => {
+      const action: ExperimentAction = {
+        type: 'updateExperimentName',
+        payload: 'Muffins',
+      }
 
-    const action: ExperimentAction = {
-      type: 'deleteCategorialVariable',
-      payload,
-    }
+      expect(rootReducer(initState, action)).toEqual({
+        experiment: {
+          ...initState.experiment,
+          id: '1234',
+          info: {
+            ...initState.experiment.info,
+            name: 'Muffins',
+          },
+        },
+      })
+    })
+  })
 
-    expect(rootReducer(initState, action)).toEqual({
-      experiment: {
-        ...initState.experiment,
-        changedSinceLastEvaluation: true,
-        categoricalVariables: [],
-      },
+  describe('updateExperimentDescription', () => {
+    it('should update description', async () => {
+      const action: ExperimentAction = {
+        type: 'updateExperimentDescription',
+        payload: 'Tasty',
+      }
+
+      expect(rootReducer(initState, action)).toEqual({
+        experiment: {
+          ...initState.experiment,
+          info: {
+            ...initState.experiment.info,
+            description: 'Tasty',
+          },
+        },
+      })
+    })
+  })
+
+  describe('Variables', () => {
+    describe('addValueVariable', () => {
+      it('should add value variable', async () => {
+        const payload: ValueVariableType = {
+          type: 'continuous',
+          name: 'Flour',
+          description: 'Wet',
+          min: 300,
+          max: 400,
+        }
+
+        const action: ExperimentAction = {
+          type: 'addValueVariable',
+          payload,
+        }
+
+        expect(rootReducer(initState, action)).toEqual({
+          experiment: {
+            ...initState.experiment,
+            changedSinceLastEvaluation: true,
+            valueVariables: [
+              {
+                name: 'Water',
+                description: 'Wet',
+                type: 'continuous',
+                min: 100,
+                max: 200,
+              },
+              payload,
+            ],
+          },
+        })
+      })
+    })
+
+    describe('deleteValueVariable', () => {
+      it('should delete value variable', async () => {
+        const payload: ValueVariableType = {
+          type: 'continuous',
+          name: 'Water',
+          description: 'Wet',
+          min: 100,
+          max: 200,
+        }
+
+        const action: ExperimentAction = {
+          type: 'deleteValueVariable',
+          payload,
+        }
+
+        expect(rootReducer(initState, action)).toEqual({
+          experiment: {
+            ...initState.experiment,
+            changedSinceLastEvaluation: true,
+            valueVariables: [],
+          },
+        })
+      })
+    })
+
+    describe('addCategorialVariable', () => {
+      it('should add categorial variable', async () => {
+        const payload: CategoricalVariableType = {
+          name: 'Fat',
+          description: 'Fatty',
+          options: [],
+        }
+
+        const action: ExperimentAction = {
+          type: 'addCategorialVariable',
+          payload,
+        }
+
+        expect(rootReducer(initState, action)).toEqual({
+          experiment: {
+            ...initState.experiment,
+            changedSinceLastEvaluation: true,
+            categoricalVariables: [
+              {
+                name: 'Icing',
+                description: 'Sugary',
+                options: [],
+              },
+              payload,
+            ],
+          },
+        })
+      })
+    })
+
+    describe('deleteCategorialVariable', () => {
+      it('should delete categorical variable', async () => {
+        const payload: CategoricalVariableType = {
+          name: 'Icing',
+          description: 'Sugary',
+          options: [],
+        }
+
+        const action: ExperimentAction = {
+          type: 'deleteCategorialVariable',
+          payload,
+        }
+
+        expect(rootReducer(initState, action)).toEqual({
+          experiment: {
+            ...initState.experiment,
+            changedSinceLastEvaluation: true,
+            categoricalVariables: [],
+          },
+        })
+      })
     })
   })
 
