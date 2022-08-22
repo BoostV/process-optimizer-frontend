ccbef51fbe8bf1c1d2869193f698b54b97b219d6
Jakob Langdal <jakob.langdal@alexandra.dk>
2022-08-22 22:13:24 +0000

    Change Xi to descriptive text

--- src/components/result-data/next-experiments.tsx
+++ src/components/result-data/next-experiments.tsx
@@ -1,4 +1,4 @@
-import { Divider, Stack, TextField } from '@mui/material'
+import { Divider, Stack, TextField, Tooltip } from '@mui/material'
 import { ChangeEvent } from 'react'
 import { useExperiment } from '../../context/experiment-context'
 
@@ -42,14 +42,21 @@ export const NextExperiments = () => {
         label="Number of suggested experiments"
         onChange={handleSuggestionChange}
       />
-      <TextField
-        fullWidth
-        type="number"
-        value={experiment.optimizerConfig.xi}
-        name="Xi"
-        label="Xi"
-        onChange={handleXiChange}
-      />
+      <Tooltip
+        enterDelay={1000}
+        arrow={true}
+        placement="top-start"
+        title="Desired improvement in relation to the best result obtained thus far. Use the same scale as your scores and update this value as you gather more data. Large values result in suggestions far away from previously tried settings, small values result in suggestions close to the expected minimum of the model. As an example, if your best result is 100 and you wish to reach 25, this value should be set to 75"
+      >
+        <TextField
+          fullWidth
+          type="number"
+          value={experiment.optimizerConfig.xi}
+          name="Xi"
+          label="Desired further improvement"
+          onChange={handleXiChange}
+        />
+      </Tooltip>
     </Stack>
   )
 }
