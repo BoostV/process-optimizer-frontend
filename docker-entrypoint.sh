1d78b9deff8c9e9fd6ed88466c1f3b683ad721cf
Jakob Langdal <jakob.langdal@alexandra.dk>
2022-11-21 14:10:25 +0000

    chore: remove next remnants from docker config

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -1,10 +1,6 @@
 #!/bin/bash
 set -e
 
-if [[ -d ".next" ]]
-then
-    find .next -name '*.js' -exec sh -c "sed -e \"s^http://localhost:9090/v1.0^${API_URL}^g\" {} > /tmp/tmp_replace && mv /tmp/tmp_replace {}" \; 
-fi
 if [[ -d "/usr/share/nginx/html" ]]
 then
     find /usr/share/nginx/html -name '*.js' -exec sh -c "sed -e \"s^http://localhost:9090/v1.0^${API_URL}^g\" {} > /tmp/tmp_replace && mv /tmp/tmp_replace {}" \; 
