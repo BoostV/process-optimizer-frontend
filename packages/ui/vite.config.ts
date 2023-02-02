7b98fe12015fd9a421e608f8513dc5affd9adcd8
Jakob Langdal <jakob.langdal@alexandra.dk>
2023-02-02 22:22:56 +0000

    Adjust build system

--- packages/ui/vite.config.ts
+++ packages/ui/vite.config.ts
@@ -2,30 +2,28 @@
 import { defineConfig } from 'vite'
 import { resolve } from 'path'
 import react from '@vitejs/plugin-react'
-import alias from '@rollup/plugin-alias'
 import dts from 'vite-plugin-dts'
 
 export default defineConfig({
-  plugins: [
-    alias(),
-    dts({
-      insertTypesEntry: true,
-    }),
-    react(),
-  ],
+  plugins: [dts(), react()],
   resolve: {
     alias: [{ find: '@ui', replacement: resolve(__dirname, './src') }],
   },
   build: {
+    sourcemap: true,
+    target: 'esnext',
+    minify: false,
     lib: {
       entry: resolve(__dirname, 'src/index.ts'),
       formats: ['es'],
     },
     rollupOptions: {
-      external: ['react', 'react-dom', 'react-hook-form'],
-      output: {
-        globals: { react: 'React', 'react-dom': 'ReactDOM' },
-      },
+      external: [
+        'react',
+        'react-dom',
+        'react-hook-form',
+        '@process-optimizer-frontend/core',
+      ],
     },
     outDir: 'dist',
   },
