5c360a11f8fd465b5d1c07fa0cb9f7bc986d17f9
Jakob Langdal <jakob.langdal@alexandra.dk>
2023-02-02 22:10:37 +0000

    Update core lib

--- packages/core/vite.config.ts
+++ packages/core/vite.config.ts
@@ -2,30 +2,24 @@
 import { defineConfig } from 'vite'
 import { resolve } from 'path'
 import react from '@vitejs/plugin-react'
-import alias from '@rollup/plugin-alias'
 import dts from 'vite-plugin-dts'
 
 export default defineConfig({
-  plugins: [
-    alias(),
-    dts({
-      insertTypesEntry: true,
-    }),
-    react(),
-  ],
+  plugins: [dts(), react()],
+
   resolve: {
     alias: [{ find: '@core', replacement: resolve(__dirname, './src') }],
   },
   build: {
+    sourcemap: true,
+    target: 'esnext',
+    minify: false,
     lib: {
       entry: resolve(__dirname, 'src/index.ts'),
       formats: ['es'],
     },
     rollupOptions: {
       external: ['react', 'react-dom', 'react-hook-form'],
-      output: {
-        globals: { react: 'React', 'react-dom': 'ReactDOM' },
-      },
     },
     outDir: 'dist',
   },
