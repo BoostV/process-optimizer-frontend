3020ce702978c6f305cfb713138911834f8220b3
Jakob Langdal <jakob.langdal@alexandra.dk>
2023-02-05 09:37:30 +0000

    Add demo application

---
+++ packages/ui/src/demo/experiment-view.tsx
@@ -0,0 +1,29 @@
+import {
+  ExperimentProvider,
+  ExperimentType,
+  useExperiment,
+} from '@process-optimizer-frontend/core'
+
+import catapult from '@ui/testing/sample-data/catapult.json' assert { type: 'json' }
+
+const Experiment = () => {
+  const { dispatch, state: experiment } = useExperiment()
+
+  const loadSample = () => {
+    dispatch({ type: 'updateExperiment', payload: catapult as ExperimentType })
+  }
+  return (
+    <div>
+      <button onClick={() => loadSample()}>Load sample</button>
+      <pre>{JSON.stringify(experiment.experiment, undefined, 2)}</pre>
+    </div>
+  )
+}
+
+export const ExperimentView = () => {
+  return (
+    <ExperimentProvider experimentId="123">
+      <Experiment />
+    </ExperimentProvider>
+  )
+}
