a73a1fb16ee46e6552272f68f3bfd5f541d940f7
Jakob Langdal <jakob.langdal@alexandra.dk>
2023-02-05 20:04:10 +0000

    Add more components

--- packages/ui/src/demo/experiment-view.tsx
+++ packages/ui/src/demo/experiment-view.tsx
@@ -1,3 +1,4 @@
+import { Box, Stack } from '@mui/system'
 import {
   ExperimentProvider,
   ExperimentType,
@@ -8,6 +9,8 @@ import {
 
 import catapult from '@ui/testing/sample-data/catapult.json'
 import { DataPoints, Plots } from '..'
+import { OptimizerConfigurator } from '../features/experiment'
+import { OptimizerModel } from '../features/input-model'
 
 const Experiment = () => {
   const {
@@ -25,17 +28,48 @@ const Experiment = () => {
   return (
     <div>
       <button onClick={() => loadSample()}>Load sample</button>
-      <DataPoints
-        experimentId={experiment.id}
-        valueVariables={experiment.valueVariables}
-        categoricalVariables={experiment.categoricalVariables}
-        scoreVariables={experiment.scoreVariables}
-        dataPoints={dataPoints}
-        newestFirst={true}
-        onToggleNewestFirst={() => {}}
-        onUpdateDataPoints={() => {}}
-      />
-      <Plots experiment={experiment} isUIBig={false} onSizeToggle={() => {}} />
+      <Box>
+        <Stack spacing={2} direction="row">
+          <DataPoints
+            experimentId={experiment.id}
+            valueVariables={experiment.valueVariables}
+            categoricalVariables={experiment.categoricalVariables}
+            scoreVariables={experiment.scoreVariables}
+            dataPoints={dataPoints}
+            newestFirst={true}
+            onToggleNewestFirst={() => {}}
+            onUpdateDataPoints={() => {}}
+          />
+          <OptimizerConfigurator
+            debug={false}
+            onConfigUpdated={() => {}}
+            config={{
+              acqFunc: '',
+              baseEstimator: '',
+              initialPoints: 2,
+              kappa: 42,
+              xi: 32,
+            }}
+          />
+          <OptimizerModel
+            disabled={false}
+            onDeleteValueVariable={() => {}}
+            onDeleteCategoricalVariable={() => {}}
+            addCategoricalVariable={() => {}}
+            addValueVariable={() => {}}
+            categoricalVariables={[]}
+            valueVariables={[]}
+          />
+        </Stack>
+        <Stack spacing={2} direction="row">
+          <Plots
+            experiment={experiment}
+            isUIBig={false}
+            onSizeToggle={() => {}}
+          />
+        </Stack>
+      </Box>
+
       <pre>{JSON.stringify(experiment, undefined, 2)}</pre>
     </div>
   )
