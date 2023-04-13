9f2a71377400022bd86c989b1f4cca53f2b5cfa4
Jakob Langdal <jakob.langdal@alexandra.dk>
2023-04-13 09:12:34 +0000

    Fix linting

--- packages/eslint-config/index.js
+++ packages/eslint-config/index.js
@@ -1,3 +1,4 @@
+// eslint-disable-next-line no-undef
 module.exports = {
   env: {
     browser: true,
@@ -6,6 +7,7 @@ module.exports = {
   extends: [
     'eslint:recommended',
     'plugin:react/recommended',
+    'plugin:react-hooks/recommended',
     'plugin:@typescript-eslint/recommended',
     'plugin:react/jsx-runtime',
   ],
@@ -16,9 +18,7 @@ module.exports = {
     sourceType: 'module',
   },
   plugins: ['react', '@typescript-eslint'],
-  rules: {
-    '@typescript-eslint/no-explicit-any': 'off',
-  },
+  rules: {},
   settings: {
     react: {
       version: 'detect',
