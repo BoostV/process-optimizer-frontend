2e0717e72ef5f3a8a5f5c69d90752e5bcab1af18
Jakob Langdal <jakob.langdal@alexandra.dk>
2023-04-15 08:59:40 +0000

    Remove  zod parsing from migration step

--- packages/core/src/common/util/migration/migrations/migrateToV11.ts
+++ packages/core/src/common/util/migration/migrations/migrateToV11.ts
@@ -1,11 +1,11 @@
-import { DataEntry, experimentSchema, ExperimentType } from '@core/common/types'
+import { DataEntry, ExperimentType } from '@core/common/types'
 
 export const migrateToV11 = (json: ExperimentType): ExperimentType => {
-  return experimentSchema.parse({
+  return {
     ...json,
     info: { ...json.info, dataFormatVersion: '11' },
     dataPoints: migrateDataPoints(json.dataPoints),
-  })
+  }
 }
 
 const migrateDataPoints = (dataPoints: DataEntry[]): DataEntry[] =>
