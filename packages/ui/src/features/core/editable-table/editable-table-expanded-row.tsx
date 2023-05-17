f644d61d959f829a67be81e46bf5dd526323cd36
Jack Ord <jack.ord@alexandra.dk>
2023-05-17 07:36:31 +0000

    Add star rating to score fields

--- packages/ui/src/features/core/editable-table/editable-table-expanded-row.tsx
+++ packages/ui/src/features/core/editable-table/editable-table-expanded-row.tsx
@@ -1,19 +1,25 @@
-import { useState } from 'react'
+import { ChangeEvent, KeyboardEvent, MouseEvent, useState } from 'react'
 import * as R from 'remeda'
 import useStyles from './editable-table-expanded-row.style'
 import {
   Box,
   Button,
+  ClickAwayListener,
+  InputAdornment,
   Paper,
+  Popper,
   Table,
   TableBody,
   TableCell,
   TableHead,
   TableRow,
+  TextField,
 } from '@mui/material'
+import StarIcon from '@mui/icons-material/Star'
 import { TableDataRow } from './types'
 import { EditableTableCell } from './editable-table-cell'
 import { InfoBox } from '../info-box/info-box'
+import { StarRating } from '@ui/features/data-points/star-rating'
 
 interface EditableTableExpandedRowProps {
   colSpan: number
@@ -43,6 +49,7 @@ export const EditableTableExpandedRow = ({
   const { classes } = useStyles()
   const [editedRow, setEditedRow] = useState<TableDataRow>({ ...tableRow })
   const isModified = !R.equals(editedRow, tableRow)
+  const [anchorEl, setAnchorEl] = useState<null | HTMLElement>(null)
 
   const handleEdit = (idx: number, value: string) => {
     setEditedRow({
@@ -92,21 +99,81 @@ export const EditableTableExpandedRow = ({
                     ))}
                   </TableRow>
                   <TableRow>
-                    {editedRow.dataPoints.map((d, i) => (
-                      <EditableTableCell
-                        key={'expandedvalues' + i}
-                        value={d.value}
-                        isEditMode
-                        onChange={(value: string) => handleEdit(i, value)}
-                        options={d.options}
-                        style={{
-                          fontSize: 14,
-                          border: 'none',
-                          paddingRight:
-                            editedRow.dataPoints.length - 1 === i ? 0 : 16,
-                        }}
-                      />
-                    ))}
+                    {editedRow.dataPoints.map((d, i) => {
+                      return d.name.includes('score') ? (
+                        <TableCell
+                          key={'scorecell' + i}
+                          style={{ border: 'none', fontSize: 14 }}
+                        >
+                          <TextField
+                            size="small"
+                            value={d.value}
+                            onChange={(e: ChangeEvent<HTMLInputElement>) =>
+                              handleEdit?.(i, '' + e.target.value)
+                            }
+                            onKeyDown={(e: KeyboardEvent) => {
+                              if (e.key === 'Enter') {
+                                d.value !== undefined && handleEdit(i, d.value)
+                                setAnchorEl(null)
+                              }
+                            }}
+                            onMouseEnter={(e: MouseEvent<HTMLElement>) => {
+                              console.log('ent', e, e.currentTarget)
+                              setAnchorEl(anchorEl ? null : e.currentTarget)
+                            }}
+                            onClick={(e: MouseEvent<HTMLElement>) => {
+                              console.log('in', e, e.currentTarget)
+                              setAnchorEl(anchorEl ? null : e.currentTarget)
+                            }}
+                            InputProps={{
+                              endAdornment: (
+                                <InputAdornment position="end">
+                                  <StarIcon sx={{ color: '#faaf00' }} />
+                                </InputAdornment>
+                              ),
+                            }}
+                          />
+                          {/* TODO: mouseover should show value but not set it, show on click instead of hover? */}
+                          <ClickAwayListener
+                            onClickAway={() => setAnchorEl(null)}
+                          >
+                            <Popper
+                              open={Boolean(anchorEl)}
+                              anchorEl={anchorEl}
+                            >
+                              <Paper>
+                                <Box pt={1} pl={1} pr={1}>
+                                  <StarRating
+                                    value={Number(d.value) ?? 5}
+                                    onChange={v => {
+                                      v !== null && handleEdit(i, '' + v)
+                                      setAnchorEl(null)
+                                    }}
+                                    onHover={v =>
+                                      v !== -1 && handleEdit(i, '' + v)
+                                    }
+                                    max={10}
+                                    precision={0.1}
+                                  />
+                                </Box>
+                              </Paper>
+                            </Popper>
+                          </ClickAwayListener>
+                        </TableCell>
+                      ) : (
+                        <EditableTableCell
+                          key={'expandedvalues' + i}
+                          value={d.value}
+                          isEditMode
+                          onChange={(value: string) => handleEdit(i, value)}
+                          options={d.options}
+                          style={{
+                            fontSize: 14,
+                            border: 'none',
+                          }}
+                        />
+                      )
+                    })}
                   </TableRow>
                 </TableBody>
               </Table>
