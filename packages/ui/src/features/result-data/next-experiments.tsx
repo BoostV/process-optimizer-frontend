426c481ea78b75a777f8ab2562898225f6d19bae
Jack Ord <jack.ord@alexandra.dk>
2023-06-21 14:54:01 +0000

    Remove xi input and move suggestions input

--- packages/ui/src/features/result-data/next-experiments.tsx
+++ packages/ui/src/features/result-data/next-experiments.tsx
@@ -1,17 +1,15 @@
-import { Divider, Stack, TextField, Tooltip } from '@mui/material'
+import { TextField } from '@mui/material'
 import { ChangeEvent, FC } from 'react'
 import { ExperimentType } from '@boostv/process-optimizer-frontend-core'
 
 type Props = {
   experiment: ExperimentType
   onSuggestionChange: (suggestionCount: string) => void
-  onXiChange: (xi: number) => void
 }
 
 export const NextExperiments: FC<Props> = ({
   experiment,
   onSuggestionChange,
-  onXiChange,
 }) => {
   const suggestionCount: number =
     (experiment.extras['experimentSuggestionCount'] as number) ?? 1
@@ -20,40 +18,13 @@ export const NextExperiments: FC<Props> = ({
     e: ChangeEvent<HTMLInputElement | HTMLTextAreaElement>
   ) => onSuggestionChange(e.target.value)
 
-  const handleXiChange = (
-    e: ChangeEvent<HTMLInputElement | HTMLTextAreaElement>
-  ) => onXiChange(Number(e.target.value))
-
   return (
-    <Stack
-      direction="row"
-      spacing={2}
-      divider={<Divider orientation="vertical" flexItem />}
-    >
-      <TextField
-        fullWidth
-        type="number"
-        value={suggestionCount}
-        name="numberOfSuggestions"
-        label="Number of suggested experiments"
-        onChange={handleSuggestionChange}
-      />
-      <Tooltip
-        disableInteractive
-        enterDelay={1000}
-        arrow={true}
-        placement="top-start"
-        title="Desired improvement in relation to the best result obtained thus far. Use the same scale as your scores and update this value as you gather more data. Large values result in suggestions far away from previously tried settings, small values result in suggestions close to the expected minimum of the model. As an example, if your best result is 100 and you wish to reach 25, this value should be set to 75"
-      >
-        <TextField
-          fullWidth
-          type="number"
-          value={experiment.optimizerConfig.xi}
-          name="Xi"
-          label="Desired further improvement"
-          onChange={handleXiChange}
-        />
-      </Tooltip>
-    </Stack>
+    <TextField
+      type="number"
+      value={suggestionCount}
+      name="numberOfSuggestions"
+      label="Number of suggestions"
+      onChange={handleSuggestionChange}
+    />
   )
 }
