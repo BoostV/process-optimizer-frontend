19128c7ead38eef0ad70e639ce1094c09c50439e
Jack Ord <jack.ord@alexandra.dk>
2023-10-11 14:28:01 +0000

    Avoid using prior score names in migration

--- packages/core/src/common/util/migration/migrations/migrateToV17.ts
+++ packages/core/src/common/util/migration/migrations/migrateToV17.ts
@@ -2,20 +2,33 @@ import { ExperimentType, scoreName } from '@core/common/types'
 import { produce } from 'immer'
 
 export const migrateToV17 = (json: ExperimentType): ExperimentType => {
+  // renames all scores scores to ["scoreName", "scoreName 2"...]
   return produce(json, draft => {
     draft.info.dataFormatVersion = '17'
     draft.scoreVariables = json.scoreVariables.map((s, i) => ({
-      name: scoreName + (i > 0 ? ` ${i + 1}` : ''),
+      name: getScoreName(scoreName, i),
       description: scoreName,
       enabled: s.enabled,
     }))
-    draft.dataPoints = json.dataPoints.map(dp => ({
-      ...dp,
-      data: dp.data.map(d => ({
-        ...d,
-        //replaces 'score' with scoreName and keeps the '2' when multiobjective
-        name: d.name.replace('score', scoreName + ' ').trim(),
-      })),
-    }))
+    draft.dataPoints = json.dataPoints.map(dp => {
+      let scoreIndex = 0
+      return {
+        ...dp,
+        data: dp.data.map(d => {
+          let newName = d.name
+          if (d.type === 'score') {
+            newName = getScoreName(scoreName, scoreIndex)
+            scoreIndex++
+          }
+          return {
+            ...d,
+            name: newName,
+          }
+        }),
+      }
+    })
   })
 }
+
+const getScoreName = (name: string, index: number) =>
+  name + (index > 0 ? ` ${index + 1}` : '')
