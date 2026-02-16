d882ca63e48157b51cc449e0829c8bdb8822ae4b
Jack Ord <36296721+j-or@users.noreply.github.com>
2026-02-16 15:27:55 +0000

    Add simple 1d plot

--- packages/plots/src/demo.tsx
+++ packages/plots/src/demo.tsx
@@ -2,6 +2,7 @@ import { PNGPlot, ParetoFrontPlot } from '.'
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import { singlePng, paretoJson } from './demo-data'
+import { OneDPlot } from './one-d-plot/one-d-plot'
 
 // cast the dummy data. Real data will be zod parsed
 const pareto = paretoJson as unknown as Parameters<
@@ -12,5 +13,31 @@ ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
     <ParetoFrontPlot plot={pareto} />
     <PNGPlot plot={singlePng} />
+    <div style={{ display: 'flex' }}>
+      <OneDPlot
+        data={[
+          { x: 1, y: 2 },
+          { x: 2, y: 3 },
+          { x: 3, y: 5 },
+          { x: 4, y: 3 },
+          { x: 5, y: 1 },
+        ]}
+        height={'140px'}
+        width={'140px'}
+        type="score"
+      />
+      <OneDPlot
+        data={[
+          { x: 1, y: [4, 2] },
+          { x: 2, y: [2, 6] },
+          { x: 3, y: [3, 5] },
+          { x: 4, y: [4, 7] },
+          { x: 5, y: [10, 11] },
+        ]}
+        height={'140px'}
+        width={'140px'}
+        referenceLineX={3}
+      />
+    </div>
   </React.StrictMode>
 )
---
+++ packages/plots/src/one-d-plot/one-d-plot.tsx
@@ -0,0 +1,59 @@
+import {
+  Area,
+  AreaChart,
+  ReferenceLine,
+  ResponsiveContainer,
+  Tooltip,
+  XAxis,
+  YAxis,
+} from 'recharts'
+
+type OneDPlotProps = {
+  data: {
+    x: number
+    y: number | number[]
+  }[]
+  type?: 'score' | 'variable'
+  width?: number | string
+  maxWidth?: number | string
+  height?: number | string
+  referenceLineX?: number
+}
+
+export const OneDPlot = ({
+  width,
+  maxWidth,
+  height,
+  data,
+  referenceLineX,
+  type = 'variable',
+}: OneDPlotProps) => {
+  return (
+    <div style={{ width, maxWidth, height }}>
+      <ResponsiveContainer width="100%" height="100%">
+        <AreaChart
+          margin={{ top: 0, right: 0, bottom: 0, left: -24 }}
+          data={data}
+        >
+          <XAxis dataKey="x" />
+          <YAxis dataKey="y" />
+          <Tooltip />
+          <Area
+            type="monotone"
+            dataKey="y"
+            stroke="#none"
+            fill={type === 'score' ? '#76c7c0' : '#a3d764'}
+          />
+          {referenceLineX !== undefined && (
+            <ReferenceLine
+              x={referenceLineX}
+              stroke="black"
+              strokeWidth={2}
+              strokeDasharray="3 3"
+            />
+          )}
+        </AreaChart>
+      </ResponsiveContainer>
+    </div>
+  )
+}
